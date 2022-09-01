e09765ab7c865ca0480dd6beba93eaa20389cdd9
nguyenhuuluan1702 <nguyenhuuluantvtc@gmail.com>
2022-09-01 15:59:19 +0700

    len la len la len

---
+++ 024_Vong_Lap_do_while.cpp
@@ -0,0 +1,16 @@
+//024. Vong lap do while
+#include <stdio.h>
+int main()
+{
+	int l;
+	
+	do
+	{	
+		printf("----\n");
+		printf("MENU\n");
+		printf("Nhap 17 de thoat.\n");
+		printf("Nhap so bat ki de tiep tuc:");
+		scanf("%d", &l);
+	}while(l!=17);
+	
+}
